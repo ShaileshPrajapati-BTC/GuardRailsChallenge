11a8f83f459288bf7587e7aee416871ed9e17246
Shailesh <shailesh.prajapati@botreetechnologies.com>
2019-11-13 19:21:33 +0530

    Added Backend server configuration and created two endpoint for API

---
+++ api/schema/scanResult.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ScanResultSchema = new Schema(
+  {
+    status: { type: String, enum: ['queued', 'in_progress.', 'success', 'failure'] },
+    repositoryName: String,
+    findings: Object,
+    queuedAt: Date,
+    scanningAt: Date,
+    finishedAt: Date
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("ScanResult", ScanResultSchema);
